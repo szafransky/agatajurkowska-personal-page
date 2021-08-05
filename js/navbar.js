0ccede95617bc0868c99405d9ab14c4ff4dbd30d
Blazej Szafranski <blazejszafranski@Apples-MacBook-Pro.local>
2021-08-05 20:55:56 +0200

    header ready

--- js/navbar.js
+++ js/navbar.js
@@ -1,15 +1,21 @@
-let lastScrollpos = window.pageYOffset;
-let menuButtonCheck = document.querySelector(".menu-btn");
-console.log(menuButtonCheck);
-window.onscroll = function() {
-	let currentScrollPos = window.pageYOffset;
-	if (lastScrollpos > currentScrollPos) {
-		document.getElementById("top-nav").style.top = "0";
-        console.log(document.getElementById("top-nav"));
-	} else {
-		document.getElementById("top-nav").style.top = "-75px";
-        console.log(document.getElementById("top-nav"));
-	}
-	lastScrollpos = currentScrollPos;
-	menuButtonCheck.checked = false;
-} 
+
+// let drop_menu = document.querySelector(".menu");
+// console.log(drop_menu);
+
+
+
+// let liEl = document.querySelectorAll(".menu li");
+// console.log(liEl);
+
+// let hideMenu = (event) => {
+// 	event.stopPropagation();
+// 	drop_menu.style.display = "none";
+// }
+
+// liEl.forEach((el) => {
+// 	el.addEventListener("click", hideMenu);
+
+// })
+
+// let menuIcon = document.querySelector(".menu-icon");
+// console.log(menuIcon);
